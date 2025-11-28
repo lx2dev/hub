832423473478f31e5a4c650504c172977ebc8e2f
SlickYeet <lasse@famlam.ca>
2025-11-27 21:53:11 -0800

    feat: update external links configuration and enhance homepage with detailed items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,44 @@
-import { ArrowRightIcon } from "lucide-react"
+import { ArrowRightIcon, BookOpenIcon, LifeBuoyIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
+import { icons } from "@/components/icons"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { config } from "@/constants"
 import { getSession } from "@/server/auth/utils"
 
 export default async function HomePage() {
   const session = await getSession()
 
-  const LINKS = config.externalLinks
+  const ITEMS = [
+    {
+      description:
+        "The Lx2 App developer documentation tries to cover every aspect of using Create Lx2 App. Whether you're new or have previous experience, we recommend starting here.",
+      href: "https://create.lx2.dev",
+      icon: BookOpenIcon,
+      title: "Documentation",
+    },
+    {
+      description:
+        "Lx2 is a set of community driven projects. Join our Discord to chat or get support.",
+      href: "https://discord.gg/hVjgY5ksy8",
+      icon: icons.discord,
+      title: "Community",
+    },
+    {
+      description:
+        "Create Lx2 App - The Most Opinionated Way to Build Next.js Apps",
+      href: "https://github.com/SlickYeet/create-lx2-app",
+      icon: icons.github,
+      title: "GitHub Repository",
+    },
+    {
+      description: "Get additional support from Lx2, we're here to listen.",
+      href: "https://hub.lx2.dev/tickets",
+      icon: LifeBuoyIcon,
+      title: "Tickets",
+    },
+  ]
 
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-center selection:bg-primary selection:text-white">
@@ -42,31 +70,29 @@ export default async function HomePage() {
 
         <main className="mt-6">
           <div className="grid gap-6 lg:grid-cols-2 lg:gap-8">
-            {Object.entries(LINKS).map(
-              ([name, { description, href, icon: Icon, title }]) => (
-                // @ts-expect-error Invalid types from typed-routes
-                <Link href={href} key={name} target="_blank">
-                  <Card className="group hover:-translate-y-0.5 h-full min-h-40 ring-0 transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-primary/50 hover:border-primary/50 dark:focus-within:ring-primary/30">
-                    <CardContent className="flex items-start gap-4">
-                      <div className="flex size-12 shrink-0 items-center justify-center rounded-full bg-primary/10 sm:size-16">
-                        <Icon className="size-5 text-primary sm:size-6" />
-                      </div>
+            {ITEMS.map(({ description, href, icon: Icon, title }) => (
+              // @ts-expect-error Invalid types from typed-routes
+              <Link href={href} key={href} target="_blank">
+                <Card className="group hover:-translate-y-0.5 h-full min-h-40 ring-0 transition duration-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-primary/50 hover:border-primary/50 dark:focus-within:ring-primary/30">
+                  <CardContent className="flex items-start gap-4">
+                    <div className="flex size-12 shrink-0 items-center justify-center rounded-full bg-primary/10 sm:size-16">
+                      <Icon className="size-5 text-primary sm:size-6" />
+                    </div>
 
-                      <div>
-                        <h2 className="font-semibold text-foreground text-xl capitalize">
-                          {title}
-                        </h2>
-                        <p className="mt-4 text-muted-foreground text-sm/relaxed transition group-hover:text-foreground/80">
-                          {description}
-                        </p>
-                      </div>
+                    <div>
+                      <h2 className="font-semibold text-foreground text-xl capitalize">
+                        {title}
+                      </h2>
+                      <p className="mt-4 text-muted-foreground text-sm/relaxed transition group-hover:text-foreground/80">
+                        {description}
+                      </p>
+                    </div>
 
-                      <ArrowRightIcon className="ml-auto size-6 shrink-0 stroke-primary" />
-                    </CardContent>
-                  </Card>
-                </Link>
-              )
-            )}
+                    <ArrowRightIcon className="ml-auto size-6 shrink-0 stroke-primary" />
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
           </div>
         </main>
 
